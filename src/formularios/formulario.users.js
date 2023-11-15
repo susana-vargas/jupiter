3766acb50100ebd2293a8051ee04e7f703ed598a
Susana <hernandeznerisusanaisabel@gmail.com>
2023-11-15 12:29:51 -0600

    se agrego la propiedad de email al formulario

--- src/formularios/formulario.users.js
+++ src/formularios/formulario.users.js
@@ -4,32 +4,36 @@ export const User = () => {
   const [usuarios, setUsuarios] = useState([])
   const [nombre, setNombre] = useState('');
   const [apellido, setApellido] = useState('');
-  const [edad, setEdad] = useState();
-  const [telefono, setTelefono] = useState();
+  const [edad, setEdad] = useState('');
+  const [telefono, setTelefono] = useState('');
+  const [email, setEmail] = useState('');
 
   const entraNombre = (event) => {
     setNombre(event.target.value);
-  }
-  
+  } 
   const entraApellido = (event) => {
     setApellido(event.target.value);
   }
-
   const entraEdad = (event) => {
-    setEdad(event.target.event);
+    setEdad(event.target.value);
   }
   const entraTelefono = (event) => {
-    setTelefono(event.target.event);
+    setTelefono(event.target.value);
   }
+  const entraEmail = (event) => {
+    setEmail(event.target.value);
+  }
+  
   const envioDatos = (event) => {
     event.preventDefault();
-    const nuevoUsuario = {nombre, apellido, edad, telefono};
+    const nuevoUsuario = {nombre, apellido, edad, telefono, email};
     setUsuarios([...usuarios, nuevoUsuario]);
     console.log('Enviado');
     setNombre('');
     setApellido('');
     setEdad('');
     setTelefono('');
+    setEmail('');
   }
   return (
     <>
@@ -48,16 +52,23 @@ export const User = () => {
       <label>Telefono: 
         <input type={"number"} value={telefono} onChange={entraTelefono}/>
       </label>
+      <label>E-mail:
+        <input type={"email"} value={email} onChange={entraEmail}/>
+      </label>
       <button type="submit">Registrar</button>
     </form>
     <ul>
     {usuarios.map((usuario, index) => (
         <li key={index}>
-          <strong>Nombre:</strong> {usuario.nombre}, <strong>Apellido:</strong> {usuario.apellido}, <strong>Edad:</strong> {usuario.edad}, <strong>Telefono:</strong> {usuario.telefono}
+          <strong>Nombre:</strong> {usuario.nombre}, 
+          <strong>Apellido:</strong> {usuario.apellido},
+          <strong>Edad:</strong> {usuario.edad}, 
+          <strong>Telefono:</strong> {usuario.telefono}, 
+          <strong>E-mail</strong> {usuario.email}
         </li>
       ))}
     </ul>
-
+    
     </>
   )
 }
